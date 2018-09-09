1a745337debc9f10762360a75ea9badf07d8d028
ash <ayeshbuddima@gmail.com>
2018-09-09 21:23:36 +0530

    addSubject form edited

--- application/views/forms/addSubject.php
+++ application/views/forms/addSubject.php
@@ -10,3 +10,22 @@
     <label>Subject Code</label>
     <input name="subjectCode">
     </br>
+    </br>
+    <label>Subject Name</label>
+    <input name="subjectName">
+    </br>
+    <label>Degree</label>
+    <input name="degree">
+    </br>
+    <label>Year</label>
+    <input name="year">
+    </br>
+    <label>Semester</label>
+    <input name="semester">
+    </br>
+
+
+    <div class="form-group col-md-2">
+        <button type="submit" class="btn btn-primary btn-xl" id="sendMessageButton">add</button>
+    </div>
+</form>
