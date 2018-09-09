fe647562653ae1c090691f81960cb350714de557
ash <ayeshbuddima@gmail.com>
2018-09-09 20:37:47 +0530

    addSubject form edited

--- application/views/forms/addSubject.php
+++ application/views/forms/addSubject.php
@@ -1,6 +1,12 @@
 <form>
 
+    <h1>
+        <font color="#00008B">
+    <label>ADD SUBJECT</label>
+        </font>
+    </h1>
+    </br>
+    </br>
     <label>Subject Code</label>
     <input name="subjectCode">
     </br>
-</form>
