59342d36c3e6de8425a2728dd1ca1e4192661a19
kaneeldias <kaneeldias@hotmail.com>
2018-09-14 22:03:25 +0530

    header improvement + form fix

--- application/views/forms/login.php
+++ application/views/forms/login.php
@@ -1,22 +1,22 @@
 <div class="row">
-    <div class="col-md-4 col-md-offset-4 form_container">
+    <div class="col-md-4 mx-auto form_container">
 
         <div class="form_title">Log In</div>
 
 
-        <form class="form_content" method="POST" action="<?=base_url("login")?>">
+        <form class="column form_content" method="POST" action="<?=base_url("login")?>">
 
-            <div class="form_item">
+            <div class="form_item col-md-12">
                 <span class="form_label">Email</span>
                 <input  class="form_input" type="email" placeholder="Email" name="email"/>
             </div>
 
-            <div class="form_item">
+            <div class="form_item col-md-12">
                 <span class="form_label">Password</span>
                 <input  class="form_input" type="password" placeholder="Password" name="password"/>
             </div>
 
-            <div class="form_item">
+            <div class="form_item col-md-3">
                 <button type="submit">Submit</button>
             </div>
 
