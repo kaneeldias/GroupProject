3c743426f876540ef239fff56bac35c6ffd07e4b
ash <ayeshbuddima@gmail.com>
2019-01-03 11:31:34 +0530

    final validations

--- application/controllers/ValidatorController.php
+++ application/controllers/ValidatorController.php
@@ -143,4 +143,85 @@ class ValidatorController extends CI_Controller {
         echo json_encode(true);
 
     }
+    public function subjectCodeExists(){
+
+        if(!isset($_GET['code'])) {
+            echo json_encode("Invalid");
+            return;
+        }
+
+        $this->load->database();
+        $this->db->where("code", $_GET['code']);
+        $query = $this->db->get('subject');
+        if ($query->num_rows() > 0){
+            echo json_encode("Subject code has already been taken.");
+            return;
+        }
+        echo json_encode(true);
+
+    }
+
+    public function editSubjectCodeExists(){
+
+        if(!isset($_GET['code'])) {
+            echo json_encode("Invalid");
+            return;
+        }
+        if(!isset($_GET['subject_id'])) {
+            echo json_encode("Invalid");
+            return;
+        }
+
+        $this->load->database();
+        $this->db->where("code", $_GET['code']);
+        $this->db->where("subject_id !=", $_GET['subject_id']);
+        $query = $this->db->get('subject');
+        if ($query->num_rows() > 0){
+            echo json_encode("Subject code has already been taken.");
+            return;
+        }
+        echo json_encode(true);
+
+    }
+
+    public function groupNameExists(){
+
+        if(!isset($_GET['groupname'])) {
+            echo json_encode("Invalid");
+            return;
+        }
+
+        $this->load->database();
+        $this->db->where("name", $_GET['groupname']);
+        $query = $this->db->get('student_group');
+        if ($query->num_rows() > 0){
+            echo json_encode("Student group name has already been taken.");
+            return;
+        }
+        echo json_encode(true);
+
+    }
+
+    public function editGroupNameExists(){
+
+        if(!isset($_GET['groupname'])) {
+            echo json_encode("Invalid");
+            return;
+        }
+        if(!isset($_GET['group_id'])) {
+            echo json_encode("Invalid");
+            return;
+        }
+
+        $this->load->database();
+        $this->db->where("name", $_GET['groupname']);
+        $this->db->where("group_id !=", $_GET['group_id']);
+        $query = $this->db->get('student_group');
+        if ($query->num_rows() > 0){
+            echo json_encode("Student group name has already been taken.");
+            return;
+        }
+        echo json_encode(true);
+
+    }
 }
